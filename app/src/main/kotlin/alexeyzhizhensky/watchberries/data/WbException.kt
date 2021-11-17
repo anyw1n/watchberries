17f2fc0ce0a0f9da2113f20bef3e93ca5bbcf637
Alexey Zhizhensky <alexey.zhizhensky@gmail.com>
2021-11-17 19:00:27 +0300

    feat: add exception class & extensions

---
+++ app/src/main/kotlin/alexeyzhizhensky/watchberries/data/WbException.kt
@@ -0,0 +1,59 @@
+package alexeyzhizhensky.watchberries.data
+
+import alexeyzhizhensky.watchberries.R
+import android.content.Context
+import androidx.annotation.StringRes
+import retrofit2.HttpException
+
+sealed class WbException(
+    @StringRes val messageRes: Int,
+    override val cause: Throwable?,
+    private val pleaseTryLater: Boolean = true
+) : Exception(cause) {
+
+    fun getMessage(context: Context): String {
+        val message = context.getString(messageRes)
+        return if (pleaseTryLater) context.getString(
+            R.string.please_try_later,
+            message
+        ) else message
+    }
+
+    class InternetConnection(cause: Throwable) :
+        WbException(R.string.internet_connection_exception, cause)
+
+    class OutOfBounds(cause: Throwable) : WbException(R.string.index_out_of_bounds, cause, false)
+
+    object InvalidSku : WbException(R.string.invalid_sku, null, false)
+
+    class Unknown(cause: Throwable) : WbException(R.string.unknown_error, cause, false)
+
+    sealed class Http(
+        @StringRes messageRes: Int,
+        override val cause: HttpException,
+        pleaseTryLater: Boolean = true
+    ) : WbException(messageRes, cause, pleaseTryLater) {
+
+        class PageNotFound(cause: HttpException) : Http(R.string.page_not_found_exception, cause)
+
+        class Unavailable(cause: HttpException) : Http(R.string.unavailable_exception, cause)
+
+        class Internal(cause: HttpException) : Http(R.string.internal_server_error, cause)
+
+        class Other(cause: HttpException) : Http(R.string.unknown_server_error, cause)
+    }
+
+    sealed class Server(
+        @StringRes messageRes: Int,
+        pleaseTryLater: Boolean = true
+    ) : WbException(messageRes, null, pleaseTryLater) {
+
+        object Response : Server(R.string.server_response_exception)
+
+        object Parse : Server(R.string.server_parse_error)
+
+        object AlreadyAdded : Server(R.string.already_added_error, false)
+
+        object Database : Server(R.string.server_db_error)
+    }
+}
--- app/src/main/kotlin/alexeyzhizhensky/watchberries/utils/Extensions.kt
+++ app/src/main/kotlin/alexeyzhizhensky/watchberries/utils/Extensions.kt
@@ -1,21 +1,101 @@
 package alexeyzhizhensky.watchberries.utils
 
+import alexeyzhizhensky.watchberries.data.WbException
+import android.content.Context
+import android.text.format.DateUtils
+import android.widget.Toast
+import androidx.annotation.StringRes
+import androidx.paging.PagingState
+import com.google.android.gms.tasks.Task
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import com.google.gson.JsonPrimitive
+import com.google.gson.JsonSerializer
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.suspendCancellableCoroutine
 import retrofit2.Call
 import retrofit2.Callback
+import retrofit2.HttpException
 import retrofit2.Response
+import java.io.IOException
+import java.net.ConnectException
+import java.net.HttpURLConnection
+import java.time.LocalDateTime
+import java.time.ZoneId
+import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 
-@ExperimentalCoroutinesApi
-suspend fun <T> Call<T>.asSuspend(): Response<T> = suspendCancellableCoroutine {
-        this.enqueue(object : Callback<T> {
-            override fun onResponse(call: Call<T>, response: Response<T>) {
-                it.resume(response, null)
-            }
-
-            override fun onFailure(call: Call<T>, t: Throwable) {
-                it.resumeWithException(t)
-            }
-        })
+@OptIn(ExperimentalCoroutinesApi::class)
+suspend fun <T> Task<T>.suspend(): T = suspendCancellableCoroutine { continuation ->
+    addOnSuccessListener { continuation.resume(it) }
+    addOnFailureListener {
+        val exception = when (it) {
+            is IOException -> WbException.InternetConnection(it)
+            else -> WbException.Unknown(it)
+        }
+        continuation.resumeWithException(exception)
     }
+}
+
+fun <Key : Any, Value : Any> PagingState<Key, Value>.anchorItemOrNull() = anchorPosition?.let {
+    closestItemToPosition(it)
+}
+
+fun LocalDateTime.toMillisWithOffset() = atZone(ZoneId.systemDefault()).let {
+    (it.toEpochSecond() + it.offset.totalSeconds) * DateUtils.SECOND_IN_MILLIS
+}
+
+inline fun <reified T> GsonBuilder.registerDeserializer(
+    crossinline deserializer: (JsonElement) -> T
+): GsonBuilder {
+    registerTypeAdapter(T::class.java, JsonDeserializer { json, _, _ -> deserializer(json) })
+    return this
+}
+
+inline fun <reified T> GsonBuilder.registerSerializer(
+    crossinline serializer: (T) -> String
+): GsonBuilder {
+    registerTypeAdapter(
+        T::class.java,
+        JsonSerializer<T> { src, _, _ -> JsonPrimitive(serializer(src)) }
+    )
+    return this
+}
+
+fun Context.toast(text: String) = Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+
+fun Context.toast(@StringRes textRes: Int) =
+    Toast.makeText(this, textRes, Toast.LENGTH_SHORT).show()
+
+suspend fun <T : Any> Call<T>.suspend(): T {
+    val response = suspendResponse()
+
+    if (!response.isSuccessful) throw HttpException(response).toWbException()
+
+    return response.body() ?: throw WbException.Server.Response
+}
+
+suspend fun <T : Any> Call<T>.suspendResponse(): Response<T> = suspendCancellableCoroutine {
+    it.invokeOnCancellation { cancel() }
+    enqueue(object : Callback<T> {
+        override fun onResponse(call: Call<T>, response: Response<T>) {
+            it.resume(response)
+        }
+
+        override fun onFailure(call: Call<T>, t: Throwable) {
+            val wbException = when (t) {
+                is ConnectException -> WbException.InternetConnection(t)
+                else -> WbException.Unknown(t)
+            }
+            it.resumeWithException(wbException)
+        }
+    })
+}
+
+fun HttpException.toWbException() = when (code()) {
+    HttpURLConnection.HTTP_NOT_FOUND -> WbException.Http.PageNotFound(this)
+    HttpURLConnection.HTTP_UNAVAILABLE -> WbException.Http.Unavailable(this)
+    HttpURLConnection.HTTP_INTERNAL_ERROR -> WbException.Http.Internal(this)
+    else -> WbException.Http.Other(this)
+}
