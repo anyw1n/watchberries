fccb8a5a1aced755600d689cc24f90bcb2b214ef
Alexey Zhizhensky <alexey.zhizhensky@gmail.com>
2021-11-17 19:35:38 +0300

    feat: add blank product detail fragment

---
+++ app/src/main/kotlin/alexeyzhizhensky/watchberries/ui/ProductDetailFragment.kt
@@ -0,0 +1,34 @@
+package alexeyzhizhensky.watchberries.ui
+
+import alexeyzhizhensky.watchberries.databinding.FragmentProductDetailBinding
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ProductDetailFragment : Fragment() {
+
+    private var _binding: FragmentProductDetailBinding? = null
+    private val binding get() = _binding!!
+
+    private val args: ProductDetailFragmentArgs by navArgs()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentProductDetailBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
