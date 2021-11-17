a454c08642ad198c17cc9c19d0a4926fb61b4c45
Alexey Zhizhensky <alexey.zhizhensky@gmail.com>
2021-11-17 18:51:17 +0300

    feat: add notification manager

---
+++ app/src/main/kotlin/alexeyzhizhensky/watchberries/WbNotificationManager.kt
@@ -0,0 +1,68 @@
+package alexeyzhizhensky.watchberries
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.graphics.Bitmap
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+import javax.inject.Singleton
+import kotlin.random.Random
+
+@Singleton
+class WbNotificationManager @Inject constructor(
+    @ApplicationContext private val context: Context
+) {
+
+    private val notificationManager = NotificationManagerCompat.from(context)
+
+    init {
+        createChannel()
+    }
+
+    private fun createChannel() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) return
+
+        val name = context.getString(R.string.notification_channel_name)
+        val description = context.getString(R.string.notification_channel_description)
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+            this.description = description
+        }
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    fun buildNotificationAndPost(
+        title: String,
+        text: String,
+        pendingIntent: PendingIntent,
+        largeIcon: Bitmap?
+    ) {
+        val bigPictureStyle = NotificationCompat.BigPictureStyle()
+            .bigPicture(largeIcon)
+            .bigLargeIcon(null)
+        val notification = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_notification_icon)
+            .setColor(ContextCompat.getColor(context, R.color.primary))
+            .setColorized(true)
+            .setContentTitle(title)
+            .setContentText(text)
+            .setLargeIcon(largeIcon)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setStyle(bigPictureStyle)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .build()
+        notificationManager.notify(Random.nextInt(), notification)
+    }
+
+    private companion object {
+
+        const val CHANNEL_ID = "PRODUCT_UPDATES"
+    }
+}
